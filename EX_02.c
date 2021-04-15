1dea0e3195c6f559fcb06640f6cecc7eb9867c8e
Rafael Brandão Nunes <>
2021-04-15 22:35:42 +0000

    Atividade 2 #2

--- EX_02.c
+++ EX_02.c
@@ -3,10 +3,18 @@
 //=================================
 void enqueue_inicio(Lista *l,Produto produto){
   Celula *nova = new_celula();
-  nova->dado = produto;
-
-  l->inicio->prox = nova;
-  nova->prox = NULL;
-  l->tam++;
+  //CASO NAO HAJA UM ELEMENTO
+  if(l->inicio->prox == NULL){
+    l->inicio->prox = nova;
+    nova->dado = produto;
+    nova->prox = NULL;
+    l->tam++;
+  }
+  //CASO HAJA UM ELEMENTO
+  else{
+    nova->prox = l->inicio->prox;
+    nova->dado = produto;
+    l->inicio->prox = nova;
+  }
 }
 //=================================
